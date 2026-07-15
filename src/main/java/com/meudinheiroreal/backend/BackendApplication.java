958ddd0454c23eb32b50d1bf52e2271a56cba7da
LucineyJS <lucineyjesus.s@gmail.com>
2026-07-15 17:23:18 -0300

    :wrench: chore(main): atualiza classe principal de inicialização ajuste TimeZone

--- src/main/java/com/meudinheiroreal/backend/BackendApplication.java
+++ src/main/java/com/meudinheiroreal/backend/BackendApplication.java
@@ -1,11 +1,18 @@
 package com.meudinheiroreal.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import java.util.TimeZone;
+
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class BackendApplication {
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+	}
 
 	public static void main(String[] eloquence) {
 		SpringApplication.run(BackendApplication.class, eloquence);
